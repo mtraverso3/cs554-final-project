ee69585b53beab12568337718e831ea11c142ca0
Marcos Traverso <marcostraverso2003@gmail.com>
2025-05-12 22:01:03 -0400

    Fix signup bug

--- src/lib/userForms.ts
+++ src/lib/userForms.ts
@@ -2,23 +2,37 @@
 
 import { auth0, authenticateUser } from "@/lib/auth/auth";
 //import * as users from "@/lib/db/data/users";
-import {User} from "@/lib/db/data/schema";
+import { User } from "@/lib/db/data/schema";
 import * as userData from "@/lib/db/data/users";
 
 export async function getUserData(): Promise<string> {
-    const userObject: User = await authenticateUser();
-    return JSON.stringify(userObject);
+  const userObject: User = await authenticateUser();
+  return JSON.stringify(userObject);
 }
 
-export async function updateProfile(first: string, last: string, profilePicture: string) {
-    const userObject: User = await authenticateUser();
-    await userData.updateUser(userObject._id.toString(), first, last, profilePicture);
+export async function updateProfile(
+  first: string,
+  last: string,
+  profilePicture: string,
+) {
+  const userObject: User = await authenticateUser();
+  await userData.updateUser(
+    userObject._id.toString(),
+    first,
+    last,
+    profilePicture,
+  );
 }
 export async function getNewUser(sub: string): Promise<string> {
-    const userObject = await userData.getUserBySub(sub);
-    return JSON.stringify(userObject);
+  const userObject = await userData.getUserBySub(sub);
+  return JSON.stringify(userObject);
 }
-export async function signup(first: string, last: string, profilePicture: string): Promise<User> {
+
+export async function signup(
+  first: string,
+  last: string,
+  profilePicture: string,
+): Promise<string> {
   const session = await auth0.getSession();
   const userObject = session?.user;
   if (!userObject || !userObject.email || !userObject.sub) {
@@ -29,10 +43,10 @@ export async function signup(first: string, last: string, profilePicture: string
     userObject.sub,
     first,
     last,
-    profilePicture
+    profilePicture,
   );
   console.log(
     `User "${user.firstName} ${user.lastName}" created successfully.`,
   );
-  return user;
+  return JSON.stringify(user);
 }
