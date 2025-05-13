2313ca8e9899cf0c558b990b4760a41228494dfd
Marcos Traverso <marcostraverso2003@gmail.com>
2025-05-12 22:01:03 -0400

    Fix signup bug

--- src/components/onboardingPage.tsx
+++ src/components/onboardingPage.tsx
@@ -1,28 +1,48 @@
-'use client'
-import { signup } from '@/lib/userForms';
-import {useState} from "react";
+"use client";
+import { signup } from "@/lib/userForms";
+import { useState } from "react";
+import { redirect } from "next/navigation";
+
 export function OnboardingPage() {
-    const [first, setFirst] = useState('');
-    const [last, setLast] = useState('');
-    const theFile = URL.createObjectURL(new File(["genericpicture"], "@genericpicture.jpg", {type: "image"}));
-    const [picture, setPicture] = useState(theFile);
-    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const files = event.target.files;
-        if(files && files[0]) {
-            setPicture(URL.createObjectURL(files[0]));
-        }
+  const [first, setFirst] = useState("");
+  const [last, setLast] = useState("");
+  const theFile = URL.createObjectURL(
+    new File(["genericpicture"], "@genericpicture.jpg", { type: "image" }),
+  );
+  const [picture, setPicture] = useState(theFile);
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files && files[0]) {
+      setPicture(URL.createObjectURL(files[0]));
     }
-    return (
-        <div>
-            First Name: <input  className="outline-1" type="text" onChange={(e) => setFirst(e.target.value)} />
-
-            Last Name: <input className="outline-1" type="text" onChange={(e) => setLast(e.target.value)} />
-            <br></br>
-            Profile Picture: <input type = "file" accept="image/*" onChange={handleFileChange} /><br></br>
-            <button className="bg-black text-white outline-1 outline-gray-200 rounded p-2"
-                    onClick={() => signup(first,last, picture)}>Submit
-            </button>
-        </div>
-
-    )
+  };
+  return (
+    <div>
+      First Name:{" "}
+      <input
+        className="outline-1"
+        type="text"
+        onChange={(e) => setFirst(e.target.value)}
+      />
+      Last Name:{" "}
+      <input
+        className="outline-1"
+        type="text"
+        onChange={(e) => setLast(e.target.value)}
+      />
+      <br></br>
+      Profile Picture:{" "}
+      <input type="file" accept="image/*" onChange={handleFileChange} />
+      <br></br>
+      <button
+        className="bg-black text-white outline-1 outline-gray-200 rounded p-2"
+        onClick={() => {
+          signup(first, last, picture);
+          redirect("/user/home");
+        }}
+      >
+        Submit
+      </button>
+    </div>
+  );
 }
